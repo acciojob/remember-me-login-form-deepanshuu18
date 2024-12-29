dde1839a2f195b69e1e36870466510f5e04622bd
DEEPANSHU <deepanshufea07@gmail.com>
2024-12-29 22:47:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const rememberMe = document.getElementById('checkbox').checked;
+ if (rememberMe) {
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+  } else {
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+  }
+  alert(`Logged in as ${username}`);
+ toggleExistingUserButton();
+});
